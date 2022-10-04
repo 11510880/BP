13064eb2c0de384b67924f82943f85bbf47b53b2
chengpin <1598639194@qq.com>
2022-10-04 12:10:34 +0800

    4th commit, ut

--- belief_propagation/test/test_varaible_node_function.py
+++ belief_propagation/test/test_varaible_node_function.py
@@ -9,7 +9,8 @@ class TestVariableNodeFunction(unittest.TestCase):
         means = [-1.6, 6.6]
         vars = [0.08, 0.16]
         m, v = variable_node_function(y, sigma, [1, 1/2], means, vars)
-        print(m, v)
+        self.assertAlmostEqual(m, 4.17755, delta=0.00001)
+        self.assertAlmostEqual(v, 0.043252, delta=0.00001)
 
 
 
